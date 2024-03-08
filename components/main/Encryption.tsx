b5669b8a428204b23c372165e2d5e3ec69e9ff2a
lelegoyeng <lelegoyengs@gmail.com>
2024-03-08 14:58:29 +0700

    update

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 const Encryption = () => {
   return (
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
-      <div className="absolute w-auto h-auto top-0 z-[5] top-20">
+      <div className="absolute w-auto h-auto z-[5] top-20">
         <motion.div
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
@@ -22,7 +22,7 @@ const Encryption = () => {
         </motion.div>
       </div>
 
-      <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto top-96">
+      <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
           <Image
             src="/LockTop.png"
